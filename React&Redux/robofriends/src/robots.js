178616082a454a8d028c50c5fa4620774f56f099
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-20 12:01:07 -0500

    React Robofriends app, display cards succesfully

---
+++ React&Redux/robofriends/src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leane Graham',
+        username: 'Bret',
+        email: 'sincere@april.bitz'
+    },
+    {
+        id:2,
+        name: 'Ervin Howel',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id:3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id:4,
+        name: 'Patricia Leback',
+        username: 'Karianne',
+        email: 'Julianne.oconner@kory.org'
+    },
+    {
+        id:5,
+        name: 'Chelsey Dietriech',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+    },
+];
